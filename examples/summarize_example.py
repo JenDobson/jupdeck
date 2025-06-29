c3c7566045f37b6d26087b31bc99b5d7616d8707
Jen Dobson <jendobson@gmail.com>
2025-06-29 09:18:16 -0700

    update interface for speaker notes toggle

--- examples/summarize_example.py
+++
@@ -1,6 +0,0 @@
-"""Example usage of the notebook summarizer API."""
-
-notebook_path = "../notebooks/demo_notebook.ipynb"
-# cells = parser.extract_notebook_content(notebook_path)
-# summary = summarizer.summarize_content(cells)
-# exporter.export_to_html(summary, "summary.html")
